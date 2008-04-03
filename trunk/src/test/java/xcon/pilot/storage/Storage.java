eaf7cc536046b2fd3d223682e8a27bf71b7b56df
mohamed.loudiyi <mohamed.loudiyi@d4190d53-3e47-0410-9621-85bd965dc335>
2008-04-03 20:21:10 +0000

    added google guice

---
+++ trunk/src/test/java/xcon/pilot/storage/Storage.java
@@ -0,0 +1,27 @@
+package xcon.pilot.storage;
+
+import java.util.Set;
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(FileStorage.class)
+public abstract class Storage {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private int capacity = DEFAULT_CAPACITY;
+
+    public void setCapacity(int capacity) {
+        this.capacity = Math.max(getKeys().size(), capacity);
+    }
+
+    public int getCapacity() {
+        return Math.max(getKeys().size(), capacity);
+    }
+
+    public abstract void store(String key, Object value);
+
+    public abstract Object read(String key);
+
+    public abstract Set<String> getKeys();
+
+    public abstract String dumpContents();
+}
