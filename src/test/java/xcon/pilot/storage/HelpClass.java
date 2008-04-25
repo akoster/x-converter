8ccb8c5bfef6542c05b0b0b21b7762784ce9ad58
mohamed.loudiyi <mohamed.loudiyi@d4190d53-3e47-0410-9621-85bd965dc335>
2008-04-25 16:02:26 +0000

    THe helpclass is implemented

---
+++ src/test/java/xcon/pilot/storage/HelpClass.java
@@ -0,0 +1,28 @@
+package xcon.pilot.storage;
+
+import org.apache.log4j.Logger;
+
+public class HelpClass implements StartInterface{
+
+	 private static final Logger LOG = Logger.getLogger(HelpClass.class);
+
+	public void run() {
+	
+		handleHelp();
+		
+	}
+	
+	
+	private void handleHelp() {
+    System.out.println("Commands:");
+    System.out.println("set <key>: setCapacity");
+    System.out.println("read <key>: reads the value for the key");
+    System.out.println("store <key <value> : stores the value under the key");
+    System.out.println("dump: dumps the contents of the storage");
+    System.out.println("impl <implementation>: change the storage implementation");
+    System.out.println("quit: exits the application");
+    System.out.println("help: shows this help sheet");
+
+}
+
+}
