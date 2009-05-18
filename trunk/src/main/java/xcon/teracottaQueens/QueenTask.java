77f74789aec8ff0ceafa6dbf76ed0df2dafe2986
mohamed.loudiyi <mohamed.loudiyi@d4190d53-3e47-0410-9621-85bd965dc335>
2009-05-18 20:18:50 +0000

    TeracottaQueen poject, implementing teracotta clustering

---
+++ trunk/src/main/java/xcon/teracottaQueens/QueenTask.java
@@ -0,0 +1,13 @@
+package xcon.teracottaQueens;
+
+public class QueenTask {
+
+    int queenColumn;
+    int boardSize;
+
+    public QueenTask(int firstColumn, int boardSize) {
+        this.queenColumn = firstColumn;
+        this.boardSize = boardSize;
+    }
+
+}
