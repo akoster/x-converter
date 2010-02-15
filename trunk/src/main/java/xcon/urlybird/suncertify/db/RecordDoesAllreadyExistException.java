f21afdf3ed7a35c4ebba9a028f34a006913053a5
mohamed.loudiyi <mohamed.loudiyi@d4190d53-3e47-0410-9621-85bd965dc335>
2010-02-15 16:59:25 +0000

    een begin gemaakt een de fileAccesImplementatie van de  urlybird app

--- trunk/src/main/java/xcon/urlybird/suncertify/db/RecordDoesAllreadyExistException.java
+++ trunk/src/main/java/xcon/urlybird/suncertify/db/RecordDoesAllreadyExistException.java
@@ -1,16 +1,13 @@
 package xcon.urlybird.suncertify.db;
 
-public class RecordNotFoundException extends Exception {
-
+public class RecordDoesAllreadyExistException  extends Exception{
+    
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
 
-    public RecordNotFoundException() {
-    }
-    
-    public RecordNotFoundException(String description)
+    public RecordDoesAllreadyExistException(String description)
     {
         super(description);
     }
