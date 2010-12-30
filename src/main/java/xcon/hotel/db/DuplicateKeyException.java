be063afaecce761a33fd70af9a5f76e197d4ad17
mohamed.loudiyi <mohamed.loudiyi@d4190d53-3e47-0410-9621-85bd965dc335>
2010-12-30 17:10:28 +0000

    gui refactored, some xxx's and some Todo's fixed

--- src/main/java/xcon/hotel/db/DuplicateKeyException.java
+++ src/main/java/xcon/hotel/db/DuplicateKeyException.java
@@ -5,7 +5,6 @@ public class DuplicateKeyException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public DuplicateKeyException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public DuplicateKeyException(String description) {
--- src/main/java/xcon/hotel/db/SwingGuiException.java
+++ src/main/java/xcon/hotel/db/SwingGuiException.java
@@ -10,22 +10,18 @@ public class SwingGuiException extends Exception {
 
     public SwingGuiException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public SwingGuiException(String arg0, Throwable arg1) {
         super(arg0, arg1);
-        // TODO Auto-generated constructor stub
     }
 
     public SwingGuiException(String arg0) {
         super(arg0);
-        // TODO Auto-generated constructor stub
     }
 
     public SwingGuiException(Throwable arg0) {
         super(arg0);
-        // TODO Auto-generated constructor stub
     }
 
 }
