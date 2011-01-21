aa7f612d04740d94ddf42af08ee669db34deee96
j.a.koster@gmail.com <j.a.koster@gmail.com@d4190d53-3e47-0410-9621-85bd965dc335>
2011-01-21 18:17:50 +0000

    Some new pilot classes.

---
+++ src/main/java/xcon/pilot/UniqueId.java
@@ -0,0 +1,15 @@
+package xcon.pilot;
+
+import java.util.UUID;
+
+public class UniqueId {
+	public static void main(String[] args) {
+
+		for (int n = 0; n < 10; n++) {
+
+			UUID uuid = UUID.randomUUID();
+			System.out.println(n + " uuid=" + uuid + " hashCode="
+					+ uuid.hashCode());
+		}
+	}
+}
