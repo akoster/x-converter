289cc54513b38ca3dd827205021357bf8ed9a697
j.a.koster <j.a.koster@d4190d53-3e47-0410-9621-85bd965dc335>
2011-09-11 20:47:43 +0000

    Added some pilots

---
+++ trunk/src/main/java/xcon/pilot/JonSkeetCounter.java
@@ -0,0 +1,27 @@
+package xcon.pilot;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+public class JonSkeetCounter {
+
+	public static void main(String[] args) throws IOException {
+
+		String jonSkeet = "Jon Skeet";
+		String q = "How much Jon Skeets does a Jon Skeet Skeet when a Jon Skeet Skeets Jon Skeets?";
+		StringReader reader = new StringReader(q);
+		int c;
+		int t = 0;
+		int jonSkeetsFound = 0;
+		while ((c = reader.read()) != -1) {
+			if (c == jonSkeet.charAt(t)) {
+				t++;
+				if (t == jonSkeet.length()) {
+					t = 0;
+					jonSkeetsFound++;
+				}
+			}
+		}
+		System.out.println("Jon Skeets found: " + jonSkeetsFound);
+	}
+}
