e08382afee5624afe53adffae9e974d51f71afaf
j.a.koster@gmail.com <j.a.koster@gmail.com@d4190d53-3e47-0410-9621-85bd965dc335>
2014-03-19 20:38:50 +0000

    Repackaged

---
+++ trunk/src/main/java/xcon/pilot/xml/Child.java
@@ -0,0 +1,27 @@
+package xcon.pilot.xml;
+
+public class Child {
+
+	private String key;
+	private String value;
+	
+	public Child(String key) {
+		this.key = key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
