43bf91814dfa86cda3992fc37f27d04cb43e049a
Adriaan Koster <info@adriaankoster.nl>
2018-12-07 14:25:50 +0100

    Update IncrementOperation.java

--- src/main/java/xcon/stackoverflow/matrix/IncrementOperation.java
+++ src/main/java/xcon/stackoverflow/matrix/IncrementOperation.java
@@ -14,13 +14,14 @@ public class IncrementOperation extends Operation {
 
 	@Override
 	void execute() {
-
-		for (int i = 0; i < cells().length; i++) {
-
-			cells()[i].setValue(cells()[i].getValue() + increment);
+		Cell[] cells = cells();
+		for (int i = 0; i < cells.length; i++) {
+			Cell cell = cells[i];
+			cell.setValue(cell.getValue() + increment);
 			try {
 				sleep(30);
 			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
 			}
 		}
 		System.out.println("Incremented " + Arrays.toString(cells()));
